e82bf960eb9f99609a41e179873cff0775f66892
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-14 19:20:56 +0100

    request location permissions

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
+import { requestPermissionsAsync } from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
+  const [error, setError] = useState(null);
+
+  const startWatching = async () => {
+    let { status } = await requestPermissionsAsync();
+    if (status !== "granted") {
+      setError("Please allow location permission to use the app");
+    } else {
+      setError(null);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {error ? <Text>{error}</Text> : null}
     </SafeAreaView>
   );
 };
